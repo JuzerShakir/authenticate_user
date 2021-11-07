3e887f6c9348fc8f0eac9318dcfe9939ae06366e
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-08 01:58:26 +0530

    bug fix shwoing recaptcha flash message after refresh and visitor signing in without checking recaptcha

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,16 @@
 class SessionsController < Devise::SessionsController
-  before_action :check_captcha, only: [:create] # Change this to be any actions you want to protect.
+  prepend_before_action :check_captcha, only: [:create] # Change this to be any actions you want to protect.
 
   private
 
   def check_captcha
-    self.resource = resource_class.new sign_in_params
-
-    if verify_recaptcha.nil?
-      flash.discard(:recaptcha_error)# We need to discard flash to avoid showing it on the next page reload
-      render :new
+    if verify_recaptcha
+      true
     else
+      self.resource = resource_class.new(sign_in_params)
+      respond_with_navigational(resource) { render :new }
+      return # this runs flash message
     end
   end
 end
+
