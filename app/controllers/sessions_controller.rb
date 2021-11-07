67404fabef6e6a1eeb44864d1aa93dfb5585725a
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-08 01:01:19 +0530

    Update controller logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,15 @@
 class SessionsController < Devise::SessionsController
-  prepend_before_action :check_captcha, only: [:create] # Change this to be any actions you want to protect.
+  before_action :check_captcha, only: [:create] # Change this to be any actions you want to protect.
 
   private
 
   def check_captcha
-    return if !verify_recaptcha
-
     self.resource = resource_class.new sign_in_params
 
-    respond_with_navigational(resource) do
-      flash.discard(:recaptcha_error) # We need to discard flash to avoid showing it on the next page reload
+    if verify_recaptcha.nil?
+      flash.discard(:recaptcha_error)# We need to discard flash to avoid showing it on the next page reload
       render :new
+    else
     end
   end
 end
