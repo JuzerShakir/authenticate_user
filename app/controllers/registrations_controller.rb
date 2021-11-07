9bcbbad58993c6fdedd55cba65a4098cb81b22b4
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-07 20:47:23 +0530

    removed "visitor" from url and updated view of validation message of recaptcha

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def check_captcha
-    return if verify_recaptcha # verify_recaptcha(action: 'signup') for v3
+    return if verify_recaptcha
 
     self.resource = resource_class.new sign_up_params
     resource.validate # Look for any other validation errors besides reCAPTCHA
