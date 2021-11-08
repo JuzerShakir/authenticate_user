fb3f0284fcedc6a03b4b9af2090117070c2a6e40
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-08 10:43:20 +0530

    removed return key

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,6 @@ class SessionsController < Devise::SessionsController
     else
       self.resource = resource_class.new(sign_in_params)
       respond_with_navigational(resource) { render :new }
-      return # this runs flash message
     end
   end
 end
