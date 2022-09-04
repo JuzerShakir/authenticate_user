9809d3684487b331de186c302176c68823db5806
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-09-05 01:26:28 +0530

    On Home page, the content for signed-in visitors: success

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.feature "On Home Page, the content", type: :feature do
+    let!(:existing_visitor) { create :visitor }
+
     before { visit root_path }
 
     scenario "for non-signed-in visitors" do
@@ -11,5 +13,14 @@ RSpec.feature "On Home Page, the content", type: :feature do
         within("h2") { expect(page).to have_content("Hey Devs, Welcome to my Second Rails Project") }
     end
 
-
+    scenario "for signed-in visitors" do
+        sign_in_with(existing_visitor)
+        expect(page).to have_content("Signed in successfully.")
+        within("header.navbar") do
+            expect(page).to have_link "Log Out", href: destroy_visitor_session_path
+        end
+        within("h2") do
+            expect(page).to have_content("Welcome, #{existing_visitor.email}")
+        end
+    end
 end
