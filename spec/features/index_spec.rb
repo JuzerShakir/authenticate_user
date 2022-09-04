e9dae19c2b763acc1296537965ae427af9b116bd
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-09-05 02:36:51 +0530

    reset

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,24 +1,38 @@
 require "rails_helper"
 
-RSpec.feature "On Home Page, the content", type: :feature do
+RSpec.feature "On Home Page,", type: :feature do
     let!(:existing_visitor) { create :visitor }
-
     before { visit root_path }
 
-    scenario "for non-signed-in visitors" do
-        within("header.navbar") do
-            expect(page).to have_content("Sign Up")
-            expect(page).to have_content("Log In")
+    describe "the content" do
+        scenario "for non-signed-in visitors" do
+            within("header.navbar") do
+                expect(page).to have_content("Sign Up")
+                expect(page).to have_content("Log In")
+            end
+            within("h2") { expect(page).to have_content("Hey Devs, Welcome to my Second Rails Project") }
         end
-        within("h2") { expect(page).to have_content("Hey Devs, Welcome to my Second Rails Project") }
+
+        scenario "for signed-in visitors" do
+            sign_in_with(existing_visitor)
+            expect(page).to have_content("Signed in successfully.")
+            within("header.navbar") do
+                expect(page).to have_link "Log Out", href: destroy_visitor_session_path
+            end
+            within("h2") { expect(page).to have_content("Welcome, #{existing_visitor.email}") }
+        end
     end
 
-    scenario "for signed-in visitors" do
-        sign_in_with(existing_visitor)
-        expect(page).to have_content("Signed in successfully.")
-        within("header.navbar") do
-            expect(page).to have_link "Log Out", href: destroy_visitor_session_path
+    describe "log out" do
+        before do
+            sign_in_with(existing_visitor)
+            click_on "Log Out"
         end
-        within("h2") { expect(page).to have_content("Welcome, #{existing_visitor.email}") }
+
+        scenario "should log out visitor" do
+            expect(page.current_path).to eq(root_path)
+            expect(page).to have_content("Signed out successfully.")
+        end
     end
+
 end
