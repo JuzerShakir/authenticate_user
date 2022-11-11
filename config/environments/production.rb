16614ac8e20ed650ecf1371271f678828bdbd99e
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-11-11 11:30:54 +0530

    rename project

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rails_authenticate_production"
+  # config.active_job.queue_name_prefix = "authenticate_user_production"
 
   config.action_mailer.perform_caching = false
 
